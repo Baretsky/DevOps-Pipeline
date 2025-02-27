aed9bbffa40aefbeaa8004de9ed5c221d1cf7dc1
Baretsky <92044788+Baretsky@users.noreply.github.com>
2025-02-27 17:33:51 +0100

    removed unused import

--- src/main/java/com/example/calculatrice/demo/CalculatriceApplication.java
+++ src/main/java/com/example/calculatrice/demo/CalculatriceApplication.java
@@ -1,6 +1,5 @@
 package com.example.calculatrice;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
